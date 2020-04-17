508fc5a77c6881d9b61576b5f46be398a8a3777b
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-04-17 12:14:45 -0400

    :sparkles: Update ogre.rb
    Update ogre.rb

---
+++ mythical-creatures/lib/ogre.rb
@@ -0,0 +1,61 @@
+class Ogre
+  attr_reader :name, :home, :encounter_counter, :swings
+
+  def initialize(name, home = "Swamp")
+    @name              = name
+    @home              = home
+    @encounter_counter = 0
+    @swings            = 0
+  end
+
+  def encounter(human)
+    @encounter_counter += 1
+    human.encounter_ogre
+    self.swing_at(human)
+  end
+
+  def swing_at(human)
+    if human.notices_ogre? == true
+      @swings += 1
+      human.swinged_at?
+    end
+  end
+
+  def apologize(human)
+    human.hear_apology
+  end
+end
+
+class Human
+  attr_reader :name, :encounter_counter
+
+  def initialize(name = "Jane")
+    @name              = name
+    @encounter_counter = 0
+    @ogre_swings       = 0
+    @knocked_out       = false
+    @notices_ogre      = false
+  end
+
+  def encounter_ogre
+    @encounter_counter += 1
+  end
+
+  def notices_ogre?
+    @notices_ogre = true if encounter_counter % 3 == 0
+  end
+
+  def swinged_at?
+    @ogre_swings += 1
+    @knocked_out = true if @ogre_swings > 0 && @ogre_swings.even?
+  end
+
+  def hear_apology
+    @knocked_out = false
+  end
+
+  def knocked_out?
+    @knocked_out
+  end
+
+end
